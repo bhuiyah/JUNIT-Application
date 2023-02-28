cea4bc6c4ff4128655179649ed7616d1aa1ea07b
bhuiyah <bhuiyansajid8@gmail.com>
2023-02-28 01:04:40 -0600

    Starting to understand what to do, kinda

--- src/assignment4/driver/TestDriver.java
+++ src/assignment4/driver/TestDriver.java
@@ -4,8 +4,12 @@ import assignment4.runners.TestRunner;
 
 public class TestDriver {
 
-    public static void runTests(String[] testclasses) {
+    public static void runTests(String[] testclasses) throws ClassNotFoundException {
         // TODO: complete this method
+        for(String testclass: testclasses){
+            Class test = Class.forName(testclass);
+            TestRunner runner = new TestRunner(test);
+        }
         // We will call this method from our JUnit test cases.
     }
 
