ebe9039cf00bd9675a79026876ead3717f36812a
bhuiyah <bhuiyansajid8@gmail.com>
2023-03-04 11:47:11 -0600

    Started a framework that might work. Definitely needs testing

--- src/assignment4/driver/TestDriver.java
+++ src/assignment4/driver/TestDriver.java
@@ -1,20 +1,50 @@
 package assignment4.driver;
 
+import assignment4.results.TestClassResult;
+import assignment4.results.TestMethodResult;
 import assignment4.runners.TestRunner;
+import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class TestDriver {
 
-    public static void runTests(String[] testclasses) throws ClassNotFoundException {
-        // TODO: complete this method
-        for(String testclass: testclasses){
-            Class test = Class.forName(testclass);
+    public static void runTests(String[] testClasses) throws ClassNotFoundException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        TestClassResult[] results = new TestClassResult[testClasses.length];
+        int i = 0;
+        for(String testClass: testClasses){
+            Class<?> test = Class.forName(testClass);
             TestRunner runner = new TestRunner(test);
+            results[i] = runner.run();
+            i++;
+        }
+        for(TestClassResult result : results){
+            for(TestMethodResult method : result.getTestMethodResults()){
+                if(method.isPass()){
+                    System.out.println("test." + result.getTestClassName() + "." + method.getName() + " : PASS");
+                }
+                else{
+                    System.out.println("test." + result.getTestClassName() + "." + method.getName() + " : FAIL");
+                }
+            }
         }
         // We will call this method from our JUnit test cases.
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, InvocationTargetException {
         // Use this for your testing.  We will not be calling this method.
         runTests(args);
     }
 }
+
+
+//terminal to specify what classes to run
+//(string) - test class use java reflection library to pull the class to our files
+//test class is found
+//we can create instances of the class
+//
+/*
+Class<?> temp = Class.forName(str)
+temp
+
+ */
--- src/assignment4/runners/TestRunner.java
+++ src/assignment4/runners/TestRunner.java
@@ -1,17 +1,41 @@
 package assignment4.runners;
 
+import assignment4.annotations.Test;
+import assignment4.assertions.Assert;
+import assignment4.assertions.AssertionException;
 import assignment4.listeners.TestListener;
 import assignment4.results.TestClassResult;
+import assignment4.results.TestMethodResult;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class TestRunner {
+    Class<?> testClass;
 
     public TestRunner(Class testClass) {
         // TODO: complete this constructor
+        this.testClass = testClass;
     }
 
-    public TestClassResult run() {
+    public TestClassResult run() throws InstantiationException, IllegalAccessException, InvocationTargetException, AssertionException {
         // TODO: complete this method
-        return null;
+        TestClassResult classResult = new TestClassResult(testClass.getName());
+        for(Method method: testClass.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(Test.class)) {
+                Object obj = testClass.newInstance();
+                TestMethodResult methodResult;
+                try {
+                    method.invoke(obj);
+                    methodResult = new TestMethodResult(method.getName(), true, null);
+                }
+                catch(AssertionException E){
+                    methodResult = new TestMethodResult(method.getName(), false, E);
+                }
+                classResult.addTestMethodResult(methodResult);
+            }
+        }
+        return classResult;
     }
 
     public void addListener(TestListener listener) {
