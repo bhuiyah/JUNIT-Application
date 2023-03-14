883861f9da7ef270dc1cd91c225e982338d03917
bhuiyah <bhuiyansajid8@gmail.com>
2023-03-14 16:41:39 -0500

    added starter files for project 5

---
+++ src/assignment4/gui/TestGUI.java
@@ -0,0 +1,16 @@
+package assignment4.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TestGUI extends Application {
+
+    @Override
+    public void start(Stage applicationStage) {
+        // TODO: Implement this method
+    }
+
+    public static void main(String[] args) {
+        launch(args); // Launch application
+    }
+}
--- src/assignment4/listeners/GUITestListener.java
+++ src/assignment4/listeners/GUITestListener.java
@@ -1,13 +1,19 @@
 package assignment4.listeners;
 
-public interface TestListener {
+public class GUITestListener implements TestListener {
 
     // Call this method right before the test method starts running
-    public void testStarted();
+    @Override
+    public void testStarted() {
+    }
 
     // Call this method right after the test method finished running successfully
-    public void testSucceeded();
+    @Override
+    public void testSucceeded() {
+    }
 
     // Call this method right after the test method finished running and failed
-    public void testFailed();
+    @Override
+    public void testFailed() {
+    }
 }
